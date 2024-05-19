98326318f4926368046f2be1841c2f5ab53def6f
Federex A Potolin <36563535+sirexlangnmn@users.noreply.github.com>
2024-05-20 04:14:15 +0800

    iosPlaySound2
    iosPlaySound2

---
+++ public/js/iosPlaySound2.js
@@ -0,0 +1,19 @@
+let lastInteraction = Date.now();
+
+function simulateInteraction() {
+    lastInteraction = Date.now();
+}
+
+
+
+// Simulate interaction on any document click
+document.addEventListener('click', simulateInteraction);
+
+// Periodically check for interaction and play sound
+setInterval(() => {
+    const timeSinceLastInteraction = Date.now() - lastInteraction;
+    const thirtyMinutes = 30 * 60 * 1000; // 30 minutes in milliseconds
+    if (timeSinceLastInteraction <= thirtyMinutes) {
+        playSound('addPeer2');
+    }
+}, 1000); // Check every second
